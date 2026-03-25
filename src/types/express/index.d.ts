fb2983367e64f1e5a68e458c8ff29b9b5495f63a
mahesh.sanraj <mahesh.sanraj@gmail.com>
2026-03-25 13:45:57 +0530

    Fix

---
+++ src/types/express/index.d.ts
@@ -0,0 +1,19 @@
+import { Request } from "express";
+import { Tenant } from "../models/tenant.model";
+import { Shop } from "../models/shop.model";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: {
+        id: string;
+        role: string;
+        tenantId?: string;
+        shopId?: string;
+        tokenVersion?: number;
+      };
+      tenant?: Tenant;
+      shop?: Shop;
+    }
+  }
+}
